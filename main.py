5aaa1d752fa3848b6d95f0cc58624173178c5f3e
imedina99 <157772884+imedina99@users.noreply.github.com>
2024-02-23 19:40:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 def main():
 
-    original_str = 'Python Programming'
-    ##################################################
-    # Comlete your code here
-    ##################################################
 
-    print(sub2)
+    original_str = "Python Programming"
+    sub1 = original_str[:6]
+    sub2 = original_str[6:]
+    merged_str = sub1 + sub2
+
     print(sub1)
+    print(sub2)
     print(merged_str)
-
-    #########################################
-    # Do not delete the return statement
     return sub1, sub2, merged_str
 
 
