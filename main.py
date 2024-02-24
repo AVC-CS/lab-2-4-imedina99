3b0137ae16e5e204c90bf2b79e6bdf2a3628a56b
imedina99 <157772884+imedina99@users.noreply.github.com>
2024-02-23 21:23:24 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ def main():
 
 
     original_str = "Python Programming"
+    sub2 = original_str[7:]
     sub1 = original_str[:6]
-    sub2 = original_str[6:]
-    merged_str = sub1 + sub2
+    merged_str = sub2 + ' ' + sub1
 
-    print(sub1)
     print(sub2)
+    print(sub1)
     print(merged_str)
-    return sub1, sub2, merged_str
 
+
+    return sub1, sub2, merged_str
 
 if __name__ == '__main__':
     main()
